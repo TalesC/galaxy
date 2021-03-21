1d78e8ed1b0602078a9f78018ed3b2346957d7f3
TalesC <talescarvalhopereira@gmail.com>
2021-03-21 14:55:47 -0300

    created MatrixUtils

--- src/main/java/br/com/meli/galaxy/utils/MatrixUtils.java
+++ src/main/java/br/com/meli/galaxy/utils/MatrixUtils.java
@@ -8,11 +8,10 @@ public class MatrixUtils {
 	public static Boolean isCollinear(List<Point> locations) {
 		Integer matrix[][] = new Integer[locations.size()][2];
 		
-		locations.forEach(location ->{
-			matrix[locations.indexOf(location)][0] = location.x;
-			matrix[locations.indexOf(location)][1] = location.y;
-		});
-		
+		for(int x=0; x < locations.size(); x++) {
+			matrix[x][0] = locations.get(x).x;
+			matrix[x][1] = locations.get(x).y;
+		}
 		var area = applyShoelaceFormula(matrix);
 		
 		return area == 0;
@@ -23,23 +22,31 @@ public class MatrixUtils {
 		var function1 = 0;
 		var function2 = 0;
 		
-		for(int x = 0; x < matrix.length; x++) {
+		for(int x = 0; x < matrix.length - 1; x++) {
 			function1 = function1 + (matrix[x][0] * matrix[x+1][1]);
 		}
 		
-		for(int x = 0; x < matrix.length; x++) {
+		for(int x = 0; x < matrix.length - 1; x++) {
 			function2 = function2 + (matrix[x][1] * matrix[x+1][0]);
 		}
 		
-		var area = function1 - function2;
+		var area = Math.abs(function1 - function2);
 				
 		return area;
 	}
 	 
-	public static void main(String[] args) {
-			var locations = new Arrays
+	/**public static void main(String[] args) {
+			var locations = Arrays.asList(
+							new Point(2, 1),
+							new Point(3, 4),
+							new Point(5, 3),
+							new Point(4, 3),
+							new Point(2, 1));
+			
+			var isCollinear = isCollinear(locations);
+			
+			System.out.println("Is collinear: " + isCollinear);
 	}
-	
-	
+	**/	
 
 }
