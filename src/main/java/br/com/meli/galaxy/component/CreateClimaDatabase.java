0baa1b744e03c8500929268acdd077e271929916
TalesC <talescarvalhopereira@gmail.com>
2021-03-23 15:40:41 -0300

    created job to generate database

--- src/main/java/br/com/meli/galaxy/component/CreateClimaDatabase.java
+++ src/main/java/br/com/meli/galaxy/component/CreateClimaDatabase.java
@@ -2,31 +2,53 @@ package br.com.meli.galaxy.component;
 
 
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
+
+import br.com.meli.galaxy.model.ent.ClimaPrediction;
+import br.com.meli.galaxy.model.enums.PlanetNameEnum;
+import br.com.meli.galaxy.repository.ClimaPredictionRepository;
+import br.com.meli.galaxy.service.ClimaPredictionService;
 
 
 @Component
 public class CreateClimaDatabase {
 
 	private static final Logger LOG = LoggerFactory.getLogger(CreateClimaDatabase.class);
-
-   @Autowired
-   private Environment environment;
-
+	
+	@Autowired
+	private ClimaPredictionService service;
+	
+	@Autowired
+	private ClimaPredictionRepository repository;
+	
    @PostConstruct
    public void init() {
-	   LOG.info("+++++++++++++++++++ Starts CreateClimaDatabase");
-	   Arrays.asList(environment.getDefaultProfiles()).forEach(str ->{
-		   LOG.info(str);
-	   });
+	   LOG.info("------------------> Generate Predictions!");
+	   
+	   var predictions = generatePredictions();
+	   LOG.info("------------------> Total of predictions: " + predictions.size());
 	  
+	   LOG.info("------------------> Starts Persistence ... ");
+	   repository.saveAll(predictions);
+	   
+	   LOG.info("------------------> Finished Persistence! ");
+   }
+   
+   private List<ClimaPrediction> generatePredictions() {
+	   var predictions = new ArrayList<ClimaPrediction>();
+	   
+	   for (PlanetNameEnum planet : PlanetNameEnum.values()) {
+		   predictions.addAll(service.predict(10, planet));
+	   }
+	   
+	   return predictions;
    }
 }
